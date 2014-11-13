761e8ba7cd694f872f3458d5a42ba65effa1451e
Andrea Vibelli <avibelli@redhat.com>
2014-11-13 10:07:50 +0100

    Update versions for 1.0.0.Beta1 release

--- push-contacts-mobile/client/push-contacts-mobile-ios/AeroGearPush.framework/Versions/1.0.0.redhat-5/Headers/AGDeviceRegistration.h
+++ push-contacts-mobile/client/push-contacts-mobile-ios/AeroGearPush.framework/Versions/1.0.0.redhat-5/Headers/AGDeviceRegistration.h
@@ -18,27 +18,27 @@
 #import <Foundation/Foundation.h>
 #import "AGClientDeviceInformation.h"
 
-// Unified Push error constants
+// AeroGear Push error constants
 extern NSString * const AGPushErrorDomain;
 extern NSString * const AGNetworkingOperationFailingURLRequestErrorKey;
 extern NSString * const AGNetworkingOperationFailingURLResponseErrorKey;
 
 /**
- * Utility to register an iOS device with the Unified Push Server.
+ * Utility to register an iOS device with the AeroGear UnifiedPush Server.
  */
 @interface AGDeviceRegistration : NSObject
 
 /**
  * An initializer method to instantiate an AGDeviceRegistration object.
  *
- * @param url the URL of the Unified Push Server.
+ * @param url the URL of the AeroGear Push server.
  *
  * @return the AGDeviceRegistration object.
  */
 - (id) initWithServerURL:(NSURL *)url;
 
 /**
- * Registers your mobile device to the Unified Push Server so it can
+ * Registers your mobile device to the AeroGear Push server so it can
  * start receiving messages.
  *
  * @param clientInfo A block object which passes in an implementation of the AGClientDeviceInformation protocol that
